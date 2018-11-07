05911a2530b3fbf762703419708e195fe6b09210
jmacalaster <julia@defmethod.com>
2018-11-07 15:58:29 -0500

    Sequelize Connect

--- models/book.js
+++ models/book.js
@@ -1,13 +1,38 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// Requiring bcrypt for password hashing. Using the bcrypt-nodejs version as the regular bcrypt module
+// sometimes causes errors on Windows machines
+var bcrypt = require("bcrypt-nodejs");
+// Creating our User model
+module.exports = function(sequelize, DataTypes) {
+  var Book = sequelize.define("Book", {
+    // The email cannot be null, and must be a proper email before creation
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    synopsis: {
+      type: DataTypes.STRING,
+    },
+    date: {
+      type: DataTypes.STRING,
+    }
+  });
+  return Book;
+};  
 
-const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
-});
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
-const Book = mongoose.model("Book", bookSchema);
+// const bookSchema = new Schema({
+//   title: { type: String, required: true },
+//   author: { type: String, required: true },
+//   synopsis: String,
+//   date: { type: Date, default: Date.now }
+// });
 
-module.exports = Book;
+// const Book = mongoose.model("Book", bookSchema);
+
+// module.exports = Book;
--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const passport = require("./config/passport");
 
-const mongoose = require("mongoose");
 const routes = require("./routes");
-const app = express();
 const PORT = process.env.PORT || 3001;
+const db = require("./models");
 
-// Define middleware here
+// Creating express app and configuring middleware needed for authentication
+const app = express(); 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
-
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  });
 });
