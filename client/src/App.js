31397d1acb99a534b0bd8319a4fb66a2efc1797f
cefimenda <jeffymenda@gmail.com>
2019-06-19 15:24:29 -0400

    fixed the logout issue on this branch too, added semi columns and spaces

--- client/src/App.js
+++ client/src/App.js
@@ -19,13 +19,14 @@ class App extends Component {
   };
   componentDidMount() {
     if (localStorage.getItem("isAuthed") !== undefined
-      && localStorage.getItem("isAuthed") !== "undefined"
-      && localStorage.getItem("isAuthed") !== null) {
+    && localStorage.getItem("isAuthed") !== "undefined"
+    && localStorage.getItem("isAuthed") !== null) {
       this.setState({ isAuthed: true });
     } else {
       this.setState({ isAuthed: false });
     }
-  }
+  };
+  
   // Set local storage to hold the logged in boolean value 
   updateAuth = (bool, userId) => {
     this.setState({
